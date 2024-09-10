4c414d0c1f2a0b39f74434711be24d352715b600
Alice Kuonji <baijingyu226@gmail.com>
2024-09-10 20:13:30 +0800

    python test16

---
+++ test16.py
@@ -0,0 +1,30 @@
+import datetime
+import re
+
+def extract_numbers(text):
+    text_numbers = re.findall(r'\d+', text)
+    if len(text_numbers) < 3 or len(text_numbers) > 6:
+        print(f"date format error")
+    return text_numbers
+
+def numbers_to_ISO(numbers : list):
+    """numbers is a list for string"""
+    result = ""
+    for i in range(3):
+        result = result + numbers[i] + '-'
+    result = result[:-1] + 'T'
+
+    for str in numbers[3:]:
+        result = result + str + ':'
+    result = result[:-1] + '+'
+    result = result + "08:00"
+    return result
+
+def get_time(text):
+    time_ = numbers_to_ISO(extract_numbers(text))
+    result = datetime.datetime.fromisoformat(time_)
+    return result
+
+input_time = input("输入日期：\n")
+result_time = get_time(input_time)
+print(result_time.date())
